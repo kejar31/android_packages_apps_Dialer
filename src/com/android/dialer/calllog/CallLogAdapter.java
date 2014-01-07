0c24c06232eb6e105595bfb70ee8960c831141fb
Cheng Xiaolong <chengxiaolong@codeaurora.org>
2014-01-07 02:01:17 -0500

    Dialer: Add the subscription info for call log.
    Add the subscription info on the call log and call detail activity.
    
     - add spinner to select "ALL SIMS","SIM1","SIM2" to filter call log
     - add spinner to select "ALL CALLS","INCOMING CALLS ONLY",
       "OUTGOING CALLS ONLY","MISSED CALLS ONLY"
    
    Conflicts:
    	res/layout/call_log_list_item.xml
    	res/values-zh-rCN/strings.xml
    	res/values/strings.xml
    	src/com/android/dialer/CallDetailActivity.java
    	src/com/android/dialer/DialtactsActivity.java
    	src/com/android/dialer/PhoneCallDetails.java
    	src/com/android/dialer/PhoneCallDetailsHelper.java
    	src/com/android/dialer/calllog/CallDetailHistoryAdapter.java
    	src/com/android/dialer/calllog/CallLogActivity.java
    	src/com/android/dialer/calllog/CallLogAdapter.java
    	src/com/android/dialer/calllog/CallLogListItemViews.java
    	tests/src/com/android/dialer/PhoneCallDetailsHelperTest.java
    	tests/src/com/android/dialer/calllog/CallLogListItemHelperTest.java
    
    Change-Id: Ib4ad371af61e9473dfe1b10e83abd2b1acaa7ef7
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/dialer/CallDetailHeader.java

--- src/com/android/dialer/calllog/CallLogAdapter.java
+++ src/com/android/dialer/calllog/CallLogAdapter.java
@@ -23,6 +23,8 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog.Calls;
+import android.provider.ContactsContract.PhoneLookup;
+import android.telephony.MSimTelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -36,6 +38,7 @@ import com.android.common.widget.GroupingListAdapter;
 import com.android.contacts.common.ContactPhotoManager;
 import com.android.contacts.common.ContactPhotoManager.DefaultImageRequest;
 import com.android.contacts.common.util.UriUtils;
+import com.android.dialer.DialtactsActivity;
 import com.android.dialer.PhoneCallDetails;
 import com.android.dialer.PhoneCallDetailsHelper;
 import com.android.dialer.R;
@@ -130,7 +133,7 @@ public class CallLogAdapter extends GroupingListAdapter
         mAdapterHelper = new CallLogAdapterHelper(context, this,
                 contactInfoHelper, mPhoneNumberDisplayHelper);
         PhoneCallDetailsHelper phoneCallDetailsHelper = new PhoneCallDetailsHelper(
-                resources, callTypeHelper, mPhoneNumberUtilsWrapper);
+                mContext, callTypeHelper, mPhoneNumberUtilsWrapper);
         mCallLogViewsHelper =
                 new CallLogListItemHelper(
                         phoneCallDetailsHelper, mPhoneNumberDisplayHelper, resources);
@@ -226,6 +229,7 @@ public class CallLogAdapter extends GroupingListAdapter
         final long duration = c.getLong(CallLogQuery.DURATION);
         final int callType = c.getInt(CallLogQuery.CALL_TYPE);
         final String countryIso = c.getString(CallLogQuery.COUNTRY_ISO);
+        final int subscription = c.getInt(CallLogQuery.SUBSCRIPTION);
 
         final ContactInfo cachedContactInfo = getContactInfoFromCallLog(c);
 
@@ -235,7 +239,8 @@ public class CallLogAdapter extends GroupingListAdapter
         // Primary action is always to call, if possible.
         if (PhoneNumberUtilsWrapper.canPlaceCallsTo(number, numberPresentation)) {
             // Sets the primary action to call the number.
-            views.primaryActionView.setTag(IntentProvider.getReturnCallIntentProvider(number));
+            views.primaryActionView.setTag(IntentProvider.getReturnCallIntentProvider(number,
+                    subscription));
         } else {
             views.primaryActionView.setTag(null);
         }
@@ -246,12 +251,22 @@ public class CallLogAdapter extends GroupingListAdapter
                 String voicemailUri = c.getString(CallLogQuery.VOICEMAIL_URI);
                 final long rowId = c.getLong(CallLogQuery.ID);
                 views.secondaryActionButtonView.setTag(
-                        IntentProvider.getPlayVoicemailIntentProvider(rowId, voicemailUri));
+                        IntentProvider.getPlayVoicemailIntentProvider(rowId, voicemailUri, subscription));
+                views.subIconView.setVisibility(View.GONE);
             } else {
                 // Store the call details information.
                 views.secondaryActionButtonView.setTag(
                         IntentProvider.getCallDetailIntentProvider(
-                                getCursor(), c.getPosition(), c.getLong(CallLogQuery.ID), count));
+                                getCursor(), c.getPosition(), c.getLong(CallLogQuery.ID), count, 
+                                subscription));
+            
+                if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+                    views.subIconView.setVisibility(View.VISIBLE);
+                    views.subIconView.setImageDrawable(
+                            DialtactsActivity.getMultiSimIcon(mContext, subscription));
+                } else {
+                    views.subIconView.setVisibility(View.GONE);
+                }
             }
         } else {
             // No action enabled.
@@ -276,11 +291,11 @@ public class CallLogAdapter extends GroupingListAdapter
         if (TextUtils.isEmpty(name)) {
             details = new PhoneCallDetails(number, numberPresentation,
                     formattedNumber, countryIso, geocode, callTypes, date,
-                    duration);
+                    duration, subscription);
         } else {
             details = new PhoneCallDetails(number, numberPresentation,
                     formattedNumber, countryIso, geocode, callTypes, date,
-                    duration, name, ntype, label, lookupUri, photoUri, sourceType);
+                    duration, name, ntype, label, lookupUri, photoUri, sourceType, subscription);
         }
 
         final boolean isNew = c.getInt(CallLogQuery.IS_READ) == 0;
